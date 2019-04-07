ddfda044387641cb24abaeb323098822b7bf0efb
raysanz <rudysanchez93@gmail.com>
2019-04-07 14:19:35 -0700

    react boostrap added// react api funct // styling needed

---
+++ src/Components/DisplayImages.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import '../App.css';
+
+class DisplayImages extends Component {
+
+    render() {
+
+        return (
+            <div className="App">
+
+                {this.props.items.length > 0 &&
+
+                    this.props.items.map(({ images }) => (
+                        <img src={images.preview_gif.url} width="200px" height="200px" />
+                    ))
+                }
+            </div>
+        );
+    }
+}
+
+export default DisplayImages;
---
+++ src/Components/SearchBox.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import '../App.css';
+import DisplayImages from './DisplayImages.js';
+
+class SearchBox extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+                       items: [] , 
+                       value: ''
+                   };
+
+        this.handleChange = this.handleChange.bind(this);           
+    }
+
+	fetchResults(props)
+	{
+		console.log(this.state.value);
+       fetch(`https://api.giphy.com/v1/gifs/search?api_key=0d05b586e3ff4884b6dc9837d9601726&limit=20&offset=0&rating=G&lang=fr&q=`+this.state.value)
+           .then(result=>result.json())
+             .then(res =>{
+           	                this.setState( {items : res.data} )
+                           }
+           	       )
+	  }
+
+
+	handleChange(event) {
+       this.setState({value: event.target.value});
+    }
+
+    render() {
+     	     
+    return (
+      <div className="form-inline">
+         <input type="text" placeholder="Search Gifs" id="search" value={this.state.value} onChange={this.handleChange} className="form-control "/>
+         <Button 
+         type="submit" 
+         value="Submit" 
+         onClick={() => {this.fetchResults(this.state.searchQuery)}} 
+         variant="primary"
+         >
+         Submit
+         </Button> 
+         
+         <DisplayImages items={this.state.items}/>
+         
+      </div>
+    );
+  }
+}
+
+export default SearchBox;
