2a350bbbb8cdcc6e6a81d8d06dc5df97fb4b0ab5
raysanz <rudysanchez93@gmail.com>
2019-04-08 19:49:50 -0700

    adding react buttons

--- src/Components/SearchBox.js
+++ src/Components/SearchBox.js
@@ -5,48 +5,54 @@ import DisplayImages from './DisplayImages.js';
 
 class SearchBox extends Component {
 
-    constructor() {
-        super();
-        this.state = {
-                       items: [] , 
-                       value: ''
-                   };
+  constructor() {
+    super();
+    this.state = {
+      items: [],
+      value: ''
+    };
 
-        this.handleChange = this.handleChange.bind(this);           
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
 
-	fetchResults(props)
-	{
-		console.log(this.state.value);
-       fetch(`https://api.giphy.com/v1/gifs/search?api_key=0d05b586e3ff4884b6dc9837d9601726&limit=20&offset=0&rating=G&lang=fr&q=`+this.state.value)
-           .then(result=>result.json())
-             .then(res =>{
-           	                this.setState( {items : res.data} )
-                           }
-           	       )
-	  }
+  fetchResults(props) {
+    console.log(this.state.value);
+    fetch(`https://api.giphy.com/v1/gifs/search?api_key=0d05b586e3ff4884b6dc9837d9601726&limit=20&offset=0&rating=G&lang=fr&q=` + this.state.value)
+      .then(result => result.json())
+      .then(res => {
+        this.setState({ items: res.data })
+      }
+      )
+  }
 
 
-	handleChange(event) {
-       this.setState({value: event.target.value});
-    }
+  handleChange(event) {
+    this.setState({ value: event.target.value });
+  }
 
-    render() {
-     	     
+  render() {
+
     return (
       <div className="form-inline">
-         <input type="text" placeholder="Search Gifs" id="search" value={this.state.value} onChange={this.handleChange} className="form-control "/>
-         <Button 
-         type="submit" 
-         value="Submit" 
-         onClick={() => {this.fetchResults(this.state.searchQuery)}} 
-         variant="primary"
-         >
-         Submit
-         </Button> 
-         
-         <DisplayImages items={this.state.items}/>
-         
+        <input type="text" placeholder="Search Gifs" id="search" value={this.state.value} onChange={this.handleChange} className="form-control " />
+        <Button
+          type="submit"
+          value="Submit"
+          onClick={() => { this.fetchResults(this.state.searchQuery) }}
+          variant="secondary" size="lg"
+        >
+          Submit
+         </Button>
+        <Button 
+        className="info"
+        >
+        Info
+        
+        </Button>
+
+
+        <DisplayImages items={this.state.items} />
+
       </div>
     );
   }
